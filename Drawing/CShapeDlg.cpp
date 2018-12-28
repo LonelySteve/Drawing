bf62918f925f4834cb3b1f286db339ed65b984af
JLoeve <jianglufull@foxmail.com>
2018-12-28 18:26:32 +0800

    从对话框中新建CShapeDlg类

---
+++ Drawing/CShapeDlg.cpp
@@ -0,0 +1,34 @@
+﻿// CShapeDlg.cpp: 实现文件
+//
+
+#include "stdafx.h"
+#include "Drawing.h"
+#include "CShapeDlg.h"
+#include "afxdialogex.h"
+
+
+// CShapeDlg 对话框
+
+IMPLEMENT_DYNAMIC(CShapeDlg, CDialogEx)
+
+CShapeDlg::CShapeDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CSHAPE_DLG, pParent)
+{
+
+}
+
+CShapeDlg::~CShapeDlg()
+{
+}
+
+void CShapeDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CShapeDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CShapeDlg 消息处理程序
---
+++ Drawing/CShapeDlg.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+
+// CShapeDlg 对话框
+
+class CShapeDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CShapeDlg)
+
+public:
+	CShapeDlg(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CShapeDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CSHAPE_DLG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
