719ee02f310b7e057b339f378af2129014b463fb
JLoeve <jianglufull@foxmail.com>
2018-12-28 20:46:10 +0800

    完成界面控件的初始化工作

--- Drawing/CShapeDlg.cpp
+++ Drawing/CShapeDlg.cpp
@@ -24,6 +24,17 @@ CShapeDlg::~CShapeDlg()
 void CShapeDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_COMBO1, m_comboBox_shapeType);
+	DDX_Control(pDX, IDC_EDIT2, m_edit_orgX);
+	DDX_Control(pDX, IDC_EDIT4, m_edit_orgY);
+	DDX_Control(pDX, IDC_EDIT3, m_edit_widthRadiusAngle);
+	DDX_Control(pDX, IDC_EDIT5, m_edit_height);
+	DDX_Control(pDX, IDC_EDIT6, edit_text);
+	DDX_Control(pDX, IDC_EDIT1, m_edit_penWidth);
+	DDX_Control(pDX, IDC_LIST1, m_listBox_penType);
+	DDX_Control(pDX, IDC_BUTTON1, m_btn_pen_color);
+	DDX_Control(pDX, IDC_LIST2, m_listBox_brushType);
+	DDX_Control(pDX, IDC_BUTTON2, m_btn_brushColor);
 }
 
 
@@ -32,3 +43,34 @@ END_MESSAGE_MAP()
 
 
 // CShapeDlg 消息处理程序
+
+
+BOOL CShapeDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  在此添加额外的初始化
+	// 初始化 m_comboBox_shapeType
+	m_comboBox_shapeType.SetCurSel(0);
+	// 初始化 m_listBox_penType
+	m_listBox_penType.AddString(_T("PS_SOLID"));
+	m_listBox_penType.AddString(_T("PS_DASH"));
+	m_listBox_penType.AddString(_T("PS_DOT"));
+	m_listBox_penType.AddString(_T("PS_DASHDOT"));
+	m_listBox_penType.AddString(_T("PS_DASHDOTDOT"));
+	m_listBox_penType.AddString(_T("PS_NULL"));
+	m_listBox_penType.AddString(_T("PS_INSIDEFRAME"));
+	m_listBox_penType.SetCurSel(0);
+	// 初始化 
+	m_listBox_brushType.AddString(_T("HS_HORIZONTAL"));
+	m_listBox_brushType.AddString(_T("HS_VERTICAL"));
+	m_listBox_brushType.AddString(_T("HS_FDIAGONAL"));
+	m_listBox_brushType.AddString(_T("HS_BDIAGONAL"));
+	m_listBox_brushType.AddString(_T("HS_CROSS"));
+	m_listBox_brushType.AddString(_T("HS_DIAGCROSS"));
+	m_listBox_brushType.AddString(_T("HS_SOLID"));
+	m_listBox_brushType.SetCurSel(0);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 异常: OCX 属性页应返回 FALSE
+}
--- Drawing/CShapeDlg.h
+++ Drawing/CShapeDlg.h
@@ -20,4 +20,29 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
 
 	DECLARE_MESSAGE_MAP()
+protected:
+	// 选择图元类型的ComboBox
+	CComboBox m_comboBox_shapeType;
+	// 原点X的编辑框
+	CEdit m_edit_orgX;
+	// 原点Y的编辑框对象
+	CEdit m_edit_orgY;
+	// 宽度、半径、角度的编辑框
+	CEdit m_edit_widthRadiusAngle;
+	// 高度的编辑框
+	CEdit m_edit_height;
+	// 文字的编辑框
+	CEdit edit_text;
+	// 线宽的编辑框
+	CEdit m_edit_penWidth;
+	// 线型的ListBox
+	CListBox m_listBox_penType;
+	// 钢笔颜色的按钮
+	CButton m_btn_pen_color;
+	// 画刷类型的ListBox
+	CListBox m_listBox_brushType;
+	// 画刷颜色的按钮
+	CButton m_btn_brushColor;
+public:
+	virtual BOOL OnInitDialog();
 };
