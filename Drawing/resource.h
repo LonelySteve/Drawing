1c161061ecddcc1f512c1799649dc325a9d2d205
JLoeve <jianglufull@foxmail.com>
2018-12-29 08:06:36 +0800

    添加用以呈现颜色的编辑框 调整相应控件大小和位置 调整控件的Tab顺序

--- Drawing/resource.h
+++ Drawing/resource.h
@@ -29,6 +29,8 @@
 #define IDC_EDIT4                       1008
 #define IDC_EDIT5                       1009
 #define IDC_EDIT6                       1010
+#define IDC_EDIT7                       1011
+#define IDC_EDIT8                       1012
 
 // Next default values for new objects
 // 
@@ -36,7 +38,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        313
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
